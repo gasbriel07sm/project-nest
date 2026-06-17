b77b72938e5ef510027a634c4730dfda6f727c4d
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-17 16:50:57 -0300

    create TaskService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ProjectsModule } from './modules/projects/projects.module';
-import { PrismaService } from './prisma.service';
+import { ProjectsModule } from './modules/projects/projects.module'
+import { TasksModule } from './modules/tasks/tasks.module'
+import { PrismaService } from './prisma.service'
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
