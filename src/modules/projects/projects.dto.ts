fc6c4cdfe9639c89db72092a51bf35b09d0143bc
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-19 07:49:27 -0300

    fix structure the return of projectById

--- src/modules/projects/projects.dto.ts
+++ src/modules/projects/projects.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { TaskPriority, TaskStatus } from '@prisma/client'
 import { IsNotEmpty, IsString } from 'class-validator'
 
 export class ProjectRequestDTO {
@@ -13,18 +14,24 @@ export class ProjectRequestDTO {
 }
 
 export class ProjectListItemDTO {
-  @ApiProperty()
-  id!: string
+  @ApiProperty() id!: string
+  @ApiProperty() name!: string
+  @ApiProperty() description!: string
+  @ApiProperty({ format: 'date-time' }) createdAt!: string
+  @ApiProperty({ format: 'date-time' }) updateAt!: string
+}
 
-  @ApiProperty()
-  name!: string
+export class ProjectTaskDTO {
+  @ApiProperty() id!: string
+  @ApiProperty() title!: string
+  @ApiProperty({ nullable: true, required: false }) description?: string
+  @ApiProperty({ enum: TaskStatus, default: TaskStatus.TODO }) status!: string
+  @ApiProperty({ enum: TaskPriority, default: TaskPriority.MEDIUM }) priority!: string
+  @ApiProperty({ nullable: true, required: false, format: 'date-time' }) dueDate?: string
+  @ApiProperty({ format: 'date-time' }) createdAt!: string
+  @ApiProperty({ format: 'date-time' }) updateAt!: string
+}
 
-  @ApiProperty()
-  description!: string
-
-  @ApiProperty({ format: 'date-time'})
-  createdAt!: string
-
-  @ApiProperty({ format: 'date-time'})
-  updateAt!: string
+export class ProjectFullDTO extends ProjectListItemDTO {
+  @ApiProperty({ type: [ProjectTaskDTO] }) tasks!: ProjectTaskDTO[]
 }
