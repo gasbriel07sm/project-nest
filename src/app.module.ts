b2565ca87039d9ac2606a2cec7d69a818d5794ce
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-22 11:35:06 -0300

    creating Auth Module, Service, Controller, DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { CollaboratorsModule } from './modules/collaborators/collaborators.module'
+import { CommentsModule } from './modules/comments/comments.module'
 import { ProjectsModule } from './modules/projects/projects.module'
 import { TasksModule } from './modules/tasks/tasks.module'
+import { UsersModule } from './modules/users/users.module'
 import { PrismaService } from './prisma.service'
-import { UsersModule } from './modules/users/users.module';
-import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
-import { CommentsModule } from './modules/comments/comments.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ProjectsModule, TasksModule, UsersModule, CollaboratorsModule, CommentsModule],
+  imports: [ProjectsModule, TasksModule, UsersModule, CollaboratorsModule, CommentsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
