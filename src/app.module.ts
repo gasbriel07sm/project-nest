f0cd15e9a35d25b7f16f9b28bdd7cf7345c3af47
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-22 08:36:07 -0300

    Comments Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { TasksModule } from './modules/tasks/tasks.module'
 import { PrismaService } from './prisma.service'
 import { UsersModule } from './modules/users/users.module';
 import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
+import { CommentsModule } from './modules/comments/comments.module';
 
 @Module({
-  imports: [ProjectsModule, TasksModule, UsersModule, CollaboratorsModule],
+  imports: [ProjectsModule, TasksModule, UsersModule, CollaboratorsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/modules/comments/comments.controller.ts
@@ -0,0 +1,80 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common'
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiResponse,
+} from '@nestjs/swagger'
+import { ValidateResourcesIds } from '../../common/decorators/validate-resources-ids/validate-resources-ids.decorator'
+import { ValidateResourcesIdsInterceptor } from '../../common/interceptors/validate-resources-ids/validate-resources-ids.interceptor'
+import { CommentFullDTO, CommentListItemDTO, CommentRequestDTO } from './comments.dto'
+import { CommentsService } from './comments.service'
+
+@Controller({
+  version: '1',
+  path: 'projects/:projectId/tasks/:taskId/comments',
+})
+@UseInterceptors(ValidateResourcesIdsInterceptor)
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @Get()
+  @ValidateResourcesIds()
+  @ApiResponse({ type: [CommentListItemDTO], description: 'Get all comments by task' })
+  findAllByTask(@Param('taskId', ParseUUIDPipe) taskId: string) {
+    return this.commentsService.findAllByTask(taskId)
+  }
+
+  @Get(':commentId')
+  @ValidateResourcesIds()
+  @ApiOkResponse({ type: CommentFullDTO, description: 'Get comment by Id' })
+  findById(
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+    @Param('commentId', ParseUUIDPipe) commentId: string,
+  ) {
+    return this.commentsService.findById(taskId, commentId)
+  }
+
+  @Post()
+  @ValidateResourcesIds()
+  @ApiCreatedResponse({ type: CommentListItemDTO, description: 'Create a new comment' })
+  @HttpCode(HttpStatus.CREATED)
+  create(@Param('taskId', ParseUUIDPipe) taskId: string, @Body() data: CommentRequestDTO) {
+    return this.commentsService.create(taskId, data)
+  }
+
+  @Put(':commentId')
+  @ValidateResourcesIds()
+  @ApiOkResponse({ type: CommentFullDTO, description: 'Update a comment' })
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+    @Param('commentId', ParseUUIDPipe) commentId: string,
+    @Body() data: CommentRequestDTO,
+  ) {
+    return this.commentsService.update(taskId, commentId, data)
+  }
+
+  @Delete(':commentId')
+  @ValidateResourcesIds()
+  @ApiNoContentResponse({ description: 'Delete a comment' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+    @Param('commentId', ParseUUIDPipe) commentId: string,
+  ) {
+    return this.commentsService.remove(taskId, commentId)
+  }
+}
