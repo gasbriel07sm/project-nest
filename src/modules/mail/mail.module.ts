19dd6fcf7b9c55c7ffc674764370fe008a1f217e
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-23 08:02:39 -0300

    add nodemailer with Handlebars and config MailModule

---
+++ src/modules/mail/mail.module.ts
@@ -0,0 +1,34 @@
+import { MailerModule } from '@nestjs-modules/mailer'
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter'
+import { Module } from '@nestjs/common'
+import path from 'path'
+import { MailService } from './mail.service'
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
+      defaults: {
+        from: '"Curso NestJS" <no-reply@monaro.dev>',
+      },
+      template: {
+        dir: path.join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
