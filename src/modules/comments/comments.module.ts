308c958ef023cacd66a72a9939b7ef6cae62ee44
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-23 08:58:23 -0300

    implement ForgotPassword and ResetPassword

--- src/modules/comments/comments.module.ts
+++ src/modules/comments/comments.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { RequestContextService } from '../../common/service/request-context.service'
 import { PrismaService } from '../../prisma.service'
 import { CommentsController } from './comments.controller'
 import { CommentsService } from './comments.service'
 
 @Module({
   controllers: [CommentsController],
-  providers: [CommentsService, PrismaService],
+  providers: [CommentsService, PrismaService, RequestContextService],
 })
 export class CommentsModule {}
--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer'
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private readonly mailService: MailerService) {}
+
+  async sendPasswordRequest(email: string, token: string) {
+    await this.mailService.sendMail({
+      to: email,
+      subject: 'Redefinição de senha',
+      template: 'forgot-password.hbs',
+      context: {
+        url: `http://localhost:3000/v1/auth/reset-password?token=${token}`,
+      },
+    })
+  }
+}
